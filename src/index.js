fb01117a2eb35d329a60c7e499ad8a7e67c57612
alexnas <alexnas2python@gmail.com>
2020-05-03 08:55:00 +0300

    Add redux, create store, actions and reducers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import { configureStore } from './store';
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { combineReducers, createStore } from 'redux';
+
+import { todos } from './todos/reducers';
+
+const reducers = { todos };
+const rootReducer = combineReducers(reducers);
+
+export const configureStore = () => createStore(rootReducer);
