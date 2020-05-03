272575f04a1ef8bc07963e36d3225deda5effc9e
alexnas <alexnas2python@gmail.com>
2020-05-03 08:02:31 +0300

    Create TodoList, TodoItem and NewTodoItem in the page.

---
+++ src/todos/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import NewTodoForm from './NewTodoForm';
+import TodoListItem from './TodoListItem';
+import './TodoList.css';
+
+const TodoList = ({ todos = [{ text: 'Hello' }] }) => {
+  return (
+    <div className='list-wrapper'>
+      <NewTodoForm />
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
